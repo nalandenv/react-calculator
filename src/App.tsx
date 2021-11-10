54aafc739374d352feb9e4a3142ec06bd54e71d5
Nikhil Nalande <nalandenv@Nikhils-MacBook-Air.local>
2021-11-10 15:17:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <h1>Calculator App</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import Calculator from './Calculator';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Calculator/>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
